4e0f19caa136fc2e302523b2f24fd1954f2dabeb
Micael Jerry <hei.jerry.2@gmail.com>
2023-06-15 12:07:26 +0300

    chore: local storage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,6 +1,28 @@
-const useLocalStorage = () => {
+import { create } from "zustand";
 
-}
+interface LocalStorageState {
+  addItem: (item: string) => void;
+  getAllItems: () => string[];
+};
+
+const useLocalStorage = create<LocalStorageState>((set) => ({
+  addItem: (item) => {
+    localStorage.setItem(Date.now().toString(), item);
+  },
+  getAllItems: () => {
+    const items = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      if (key) {
+        const item = localStorage.getItem(key);
+        if (item) {
+          items.push(item);
+        }
+      }
+    }
+    return items;
+  }
+}));
 
 export {
   useLocalStorage
